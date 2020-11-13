309e318fdbfd1619906a726cf63a582df0b3ea2a
Edilson Rodrigues <edarodrigs@gmail.com>
2020-11-13 07:35:16 -0300

    Dicee Done

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueAccent,
         appBar: AppBar(
+          elevation: 0,
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          backgroundColor: Colors.blue[900],
         ),
         body: DicePage(),
       ),
@@ -25,37 +27,58 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
   final int maxDicesNumber = 6;
+  int result = 2;
 
   void rollDice() {
     setState(() {
       leftDiceNumber = Random().nextInt(maxDicesNumber) + 1;
       rightDiceNumber = Random().nextInt(maxDicesNumber) + 1;
+      result = leftDiceNumber + rightDiceNumber;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: FlatButton(
-              onPressed: () => rollDice(),
-              child: Image.asset(
-                'images/dice$leftDiceNumber.png',
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Card(
+          elevation: 0.5,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'He gave $result',
+              style: TextStyle(
+                fontSize: 32,
+                color: Color(0xFF212121),
               ),
             ),
           ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () => rollDice(),
-              child: Image.asset(
-                'images/dice$rightDiceNumber.png',
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        FlatButton(
+          onPressed: () => rollDice(),
+          child: Row(
+            children: [
+              Expanded(
+                child: Image.asset(
+                  'images/dice$leftDiceNumber.png',
+                ),
               ),
-            ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Expanded(
+                child: Image.asset(
+                  'images/dice$rightDiceNumber.png',
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
